71739989aca53719012f1844b9eb4fc93f67e101
Ayushmaurya <139100630+Sarcastic21@users.noreply.github.com>
2025-01-07 21:52:48 +0530

    Update Booking.js

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose';
 
-const booking2 = new mongoose.Schema({
+const bookingSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   accommodationType: { type: String, required: true },
   checkIn: { type: String, required: true },
   adults: { type: Number, required: true },
-  children: { type: Number, required: true }
+  children: { type: Number, required: true },
+  bookingPackage: { type: String, required: true },  // Renamed package to bookingPackage
+  price: { type: Number, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  payment: { type: Boolean, default: false },  // Payment status field
 });
 
-const Booking2 = mongoose.model('Booking2', booking2);
+const Booking = mongoose.model('Booking', bookingSchema);
 
-export default Booking2;
+export default Booking;
+
